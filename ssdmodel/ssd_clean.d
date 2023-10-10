c5f389b93325b47a4f351e9c86bfae2323def1e1
CherryYang <754449629@qq.com>
2023-10-10 19:49:25 +0800

    new

--- ssdmodel/ssd_clean.d
+++ ssdmodel/ssd_clean.d
@@ -3,83 +3,27 @@ ssd_clean.o: ssd_clean.c /usr/include/stdc-predef.h ssd.h \
  ../src//disksim_malloc.h ../src//disksim_bitstring.h ../src//inline.h \
  /usr/include/sys/types.h /usr/include/features.h \
  /usr/include/sys/cdefs.h /usr/include/bits/wordsize.h \
- /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
- /usr/include/gnu/stubs-64.h /usr/include/bits/types.h \
- /usr/include/bits/timesize.h /usr/include/bits/typesizes.h \
- /usr/include/bits/time64.h /usr/include/bits/types/clock_t.h \
- /usr/include/bits/types/clockid_t.h /usr/include/bits/types/time_t.h \
- /usr/include/bits/types/timer_t.h \
- /opt/gcc-11.2.0/lib/gcc/x86_64-pc-linux-gnu/11.2.0/include/stddef.h \
- /usr/include/bits/stdint-intn.h /usr/include/endian.h \
- /usr/include/bits/endian.h /usr/include/bits/byteswap.h \
- /usr/include/bits/uintn-identity.h /usr/include/sys/select.h \
- /usr/include/bits/select.h /usr/include/bits/types/sigset_t.h \
- /usr/include/bits/types/__sigset_t.h \
- /usr/include/bits/types/struct_timeval.h \
- /usr/include/bits/types/struct_timespec.h \
- /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
- /usr/include/bits/pthreadtypes-arch.h /usr/include/stdio.h \
- /usr/include/bits/libc-header-start.h \
- /opt/gcc-11.2.0/lib/gcc/x86_64-pc-linux-gnu/11.2.0/include/stdarg.h \
- /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__mbstate_t.h \
- /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
- /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
- /usr/include/bits/types/cookie_io_functions_t.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/time.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stddef.h \
+ /usr/include/endian.h /usr/include/bits/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/byteswap-16.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/sigset.h /usr/include/bits/time.h \
+ /usr/include/sys/sysmacros.h /usr/include/bits/pthreadtypes.h \
+ /usr/include/stdio.h /usr/include/libio.h /usr/include/_G_config.h \
+ /usr/include/wchar.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stdarg.h \
  /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/stdlib.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/cstdlib \
- /opt/gcc-11.2.0/include/c++/11.2.0/x86_64-pc-linux-gnu/bits/c++config.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/x86_64-pc-linux-gnu/bits/os_defines.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/x86_64-pc-linux-gnu/bits/cpu_defines.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/pstl/pstl_config.h \
  /usr/include/stdlib.h /usr/include/bits/waitflags.h \
- /usr/include/bits/waitstatus.h /usr/include/bits/floatn.h \
- /usr/include/bits/floatn-common.h /usr/include/bits/types/locale_t.h \
- /usr/include/bits/types/__locale_t.h /usr/include/alloca.h \
- /usr/include/bits/stdlib-float.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/std_abs.h /usr/include/string.h \
- /usr/include/strings.h /opt/gcc-11.2.0/include/c++/11.2.0/math.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/cmath \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/cpp_type_traits.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/ext/type_traits.h /usr/include/math.h \
- /usr/include/bits/math-vector.h /usr/include/bits/libm-simd-decl-stubs.h \
- /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
- /usr/include/bits/fp-fast.h \
- /usr/include/bits/mathcalls-helper-functions.h \
- /usr/include/bits/mathcalls.h /usr/include/bits/mathcalls-narrow.h \
- /usr/include/bits/iscanonical.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/specfun.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/stl_algobase.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/functexcept.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/exception_defines.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/ext/numeric_traits.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/stl_pair.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/move.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/type_traits \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/stl_iterator_base_types.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/stl_iterator_base_funcs.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/concept_check.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/debug/assertions.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/stl_iterator.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/ptr_traits.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/debug/debug.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/bits/predefined_ops.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/limits \
- /opt/gcc-11.2.0/include/c++/11.2.0/tr1/gamma.tcc \
- /opt/gcc-11.2.0/include/c++/11.2.0/tr1/special_function_util.h \
- /opt/gcc-11.2.0/include/c++/11.2.0/tr1/bessel_function.tcc \
- /opt/gcc-11.2.0/include/c++/11.2.0/tr1/beta_function.tcc \
- /opt/gcc-11.2.0/include/c++/11.2.0/tr1/ell_integral.tcc \
- /opt/gcc-11.2.0/include/c++/11.2.0/tr1/exp_integral.tcc \
- /opt/gcc-11.2.0/include/c++/11.2.0/tr1/hypergeometric.tcc \
- /opt/gcc-11.2.0/include/c++/11.2.0/tr1/legendre_function.tcc \
- /opt/gcc-11.2.0/include/c++/11.2.0/tr1/modified_bessel_func.tcc \
- /opt/gcc-11.2.0/include/c++/11.2.0/tr1/poly_hermite.tcc \
- /opt/gcc-11.2.0/include/c++/11.2.0/tr1/poly_laguerre.tcc \
- /opt/gcc-11.2.0/include/c++/11.2.0/tr1/riemann_zeta.tcc \
- /usr/include/time.h /usr/include/bits/time.h /usr/include/bits/timex.h \
- /usr/include/bits/types/struct_tm.h \
- /usr/include/bits/types/struct_itimerspec.h ../src//disksim_assertlib.h \
+ /usr/include/bits/waitstatus.h /usr/include/xlocale.h \
+ /usr/include/alloca.h /usr/include/bits/stdlib-float.h \
+ /usr/include/string.h /usr/include/math.h /usr/include/bits/huge_val.h \
+ /usr/include/bits/huge_valf.h /usr/include/bits/huge_vall.h \
+ /usr/include/bits/inf.h /usr/include/bits/nan.h \
+ /usr/include/bits/mathdef.h /usr/include/bits/mathcalls.h \
+ /usr/include/bits/timex.h ../src//disksim_assertlib.h \
  /usr/include/assert.h ../src//disksim_reqflags.h ../src//disksim_iosim.h \
  ../src//disksim_ioface.h ../src//disksim_global.h \
  ../src//disksim_device.h ../src//disksim_iosim.h \
@@ -88,9 +32,36 @@ ssd_clean.o: ssd_clean.c /usr/include/stdc-predef.h ssd.h \
  ../libparam/libparam.h ../libparam/bitvector.h ../libddbg/libddbg.h \
  ../src//disksim_bus.h ../src//disksim_controller.h \
  ../src//disksim_stat.h ../src//disksim_ioqueue.h ../src//disksim_disk.h \
- ../src//disksim_ioqueue.h ../diskmodel/include/diskmodel/dm.h \
+ /usr/include/c++/4.8.2/map /usr/include/c++/4.8.2/bits/stl_tree.h \
+ /usr/include/c++/4.8.2/bits/stl_algobase.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/c++config.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/os_defines.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/cpu_defines.h \
+ /usr/include/c++/4.8.2/bits/functexcept.h \
+ /usr/include/c++/4.8.2/bits/exception_defines.h \
+ /usr/include/c++/4.8.2/bits/cpp_type_traits.h \
+ /usr/include/c++/4.8.2/ext/type_traits.h \
+ /usr/include/c++/4.8.2/ext/numeric_traits.h \
+ /usr/include/c++/4.8.2/bits/stl_pair.h \
+ /usr/include/c++/4.8.2/bits/move.h \
+ /usr/include/c++/4.8.2/bits/concept_check.h \
+ /usr/include/c++/4.8.2/bits/stl_iterator_base_types.h \
+ /usr/include/c++/4.8.2/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/4.8.2/debug/debug.h \
+ /usr/include/c++/4.8.2/bits/stl_iterator.h \
+ /usr/include/c++/4.8.2/bits/allocator.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/c++allocator.h \
+ /usr/include/c++/4.8.2/ext/new_allocator.h /usr/include/c++/4.8.2/new \
+ /usr/include/c++/4.8.2/exception \
+ /usr/include/c++/4.8.2/bits/atomic_lockfree_defines.h \
+ /usr/include/c++/4.8.2/bits/memoryfwd.h \
+ /usr/include/c++/4.8.2/bits/stl_function.h \
+ /usr/include/c++/4.8.2/backward/binders.h \
+ /usr/include/c++/4.8.2/bits/stl_map.h \
+ /usr/include/c++/4.8.2/bits/stl_multimap.h \
+ /usr/include/c++/4.8.2/bits/range_access.h ../src//disksim_ioqueue.h \
+ ../diskmodel/include/diskmodel/dm.h \
  ../diskmodel/include/diskmodel/dm_types.h /usr/include/inttypes.h \
- /opt/gcc-11.2.0/lib/gcc/x86_64-pc-linux-gnu/11.2.0/include/stdint.h \
- /usr/include/stdint.h /usr/include/bits/wchar.h \
- /usr/include/bits/stdint-uintn.h ../src//disksim_bus.h ../src//config.h \
- ssd_clean.h modules/ssdmodel_ssd_param.h ssd_utils.h
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/wchar.h ../src//disksim_bus.h \
+ ../src//config.h ssd_clean.h modules/ssdmodel_ssd_param.h ssd_utils.h
